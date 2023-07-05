ecca4c7d8c379f14517a51ec81c85f7f21eec29e
jose <jalves@gogreen.co.uk>
2023-07-05 11:55:09 +0100

    transferred current work

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+
+<script src="/js/main.js"></script>
+<link rel="stylesheet" href="/css/main.css">
+<title>Hello</title>
+
+<body>
+
+    <article>
+
+        <?= $post; ?>
+
+    </article>
+
+    <button onclick="window.location.href = '/';"> Go back to mainpage </button>
+
+</body>
+
+
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<script src="/js/main.js"></script>
+<link rel="stylesheet" href="/css/main.css">
+<title>Hello</title>
+
+<body>
+
+    <h1>Hello There</h1>
+
+    <article>
+        <h1><a href="/post"> Ask me a question! </a></h1>
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dicta distinctio eveniet sit voluptate? Atque distinctio enim esse, est et ipsam magnam nesciunt nostrum perferendis veniam. Explicabo facere, nam?</p>
+    </article>
+
+
+    <article>
+        <h1><a href="/post"> Ask me a question! </a></h1>
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dicta distinctio eveniet sit voluptate? Atque distinctio enim esse, est et ipsam magnam nesciunt nostrum perferendis veniam. Explicabo facere, nam?</p>
+    </article>
+
+    <article>
+        <h1><a href="/post"> Ask me a question! </a></h1>
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur commodi corporis culpa cumque dignissimos eligendi fugit necessitatibus numquam omnis porro possimus, quae rerum tenetur unde, voluptates. Cumque iusto modi quibusdam!</p>
+    </article>
+    <button onclick="showAlert()">Show alert</button>
+
+</body>
