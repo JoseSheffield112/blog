7eaae53aa1b4da5e4d2b8250975c653b4114c917
jose <jalves@gogreen.co.uk>
2023-07-07 14:56:57 +0100

    refactored routes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,26 +6,10 @@
 
 <body>
 
-    <h1>Hello There</h1>
-
-    <article>
-        <h1><a href="/posts/my-first-post"> Ask me a question! </a></h1>
-
-        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dicta distinctio eveniet sit voluptate? Atque distinctio enim esse, est et ipsam magnam nesciunt nostrum perferendis veniam. Explicabo facere, nam?</p>
-    </article>
-
-
-    <article>
-        <h1><a href="/posts/my-second-post"> Ask me a question! </a></h1>
-
-        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dicta distinctio eveniet sit voluptate? Atque distinctio enim esse, est et ipsam magnam nesciunt nostrum perferendis veniam. Explicabo facere, nam?</p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post"> Ask me a question! </a></h1>
-
-        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur commodi corporis culpa cumque dignissimos eligendi fugit necessitatibus numquam omnis porro possimus, quae rerum tenetur unde, voluptates. Cumque iusto modi quibusdam!</p>
-    </article>
-    <button onclick="showAlert()">Show alert</button>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+                <?= $post ?>
+        </article>
+    <?php endforeach; ?>
 
 </body>
