57149930af354f8f3c3b28d4f7598bf6297b820f
jose <jalves@gogreen.co.uk>
2023-07-12 09:31:09 +0100

    refactor: implemented eloquent

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,70 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public $title;
-
-    public $excerpt;
-
-    public $date;
-
-    public $body;
-
-    public $slug;
-
-    public function __construct($title, $slug, $excerpt, $date, $body)
-    {
-        $this->title = $title;
-        $this->slug = $slug;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-    }
-    /**
-     * returns content of all files in 'resource_path/posts'
-     * @return array|string[]
-     */
-    public static function all(){
-        return cache()->remember('posts.all', now()->addMinutes(5), function (){
-            return collect(File::files(resource_path("/posts")))
-                -> map(fn($file) => YamlFrontMatter::parseFile($file))
-                -> map(fn($document) => new Post (
-                    $document->title,
-                    $document->slug,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body()
-                ))
-                -> sortByDesc('date');
-        });
-    }
-    /**
-     * Given a slug, returns a file matching that slug
-     * @param $slug
-     * @return mixed
-     */
-    public static function find($slug)
-    {
-        return (static::all()-> firstWhere('slug', $slug));
-    }
-
-    /**
-     * Given a slug, returns file matching that slug or it fails
-     * @param $slug
-     * @return mixed
-     */
-    public static function findOrFail($slug)
-    {
-        $post = static::find($slug);
-
-        if(! $post){
-            return null;
-        }
-
-        return $post;
-    }
+    use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/post/{post}', function ($slug) {
-    $post = Post::findOrFail($slug);
+Route::get('/post/{post}', function ($id) {
+    $post = Post::findOrFail($id);
     return ($post) ? view('/post', [
         'post' => $post
     ]) : Redirect::to('/');
