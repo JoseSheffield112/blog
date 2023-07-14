7dd031b995a18f095158fe5321361709aa8c2f49
jose <jalves@gogreen.co.uk>
2023-07-14 13:56:46 +0100

    refactor: cleaned up User model & factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -39,10 +40,13 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 
+    /**
+     * Eloquent function to get posts
+     * @return HasMany
+     */
     public function posts(){
         return $this->hasMany(Post::class);
     }
