42eb30b2906a015ee134c2cb6cecfcd55792237f
jose <jalves@gogreen.co.uk>
2023-07-17 16:09:27 +0100

    feature: full grid for posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,14 @@
 <x-layout>
-    <x-slot name="title">
-        All posts
-    </x-slot>
-
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-        <x-featured-post-card :post="$posts->first()"/>
-
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-
-            <x-post-card/>
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card/>
-
-            <x-post-card/>
-
-            <x-post-card/>
-        </div>
+        @if($posts->count())
+           <x-posts-grid :posts="$posts"/>
+        @else
+            <p clas="text-center">
+                There are no posts! Please check back later.
+            </p>
+        @endif
     </main>
 </x-layout>
