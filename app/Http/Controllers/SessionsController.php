fb79fd36b92f0ce298974002e76e73c0419e35b1
jose <jalves@gogreen.co.uk>
2023-07-25 14:26:30 +0100

    feature: created a login page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create(){
+        return view('sessions.create');
+    }
+
+    public function store(){
+        // authorise
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+        // login
+        if(auth()->attempt($attributes)){
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+        // redirect
+        throw ValidationException::withMessages([
+            'email' => 'This email couldn\'t be verified'
+        ]);
+    }
+
     public function destroy(){
         auth()->logout();
 
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,12 @@ Route::get('/post/{post:slug}', [PostController::class, 'show']);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::post('logout', [SessionsController::class, 'destroy']);
+//login
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+
 
 
