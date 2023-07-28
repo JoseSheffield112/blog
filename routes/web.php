b79aedf483af12642be122d8c89fb3b70be4e4a8
jose <jalves@gogreen.co.uk>
2023-07-28 16:11:19 +0100

    refactor: added admin middleware rule

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 // making posts
-Route::get('admin/posts/create', [PostController::class, 'create']);
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
 
 
 
