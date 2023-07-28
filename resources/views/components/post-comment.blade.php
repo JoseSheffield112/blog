f5cf2c087a8b9b45595ffeef32bfb5b3bc205159
jose <jalves@gogreen.co.uk>
2023-07-28 09:32:30 +0100

    refactor: ubiquitous avatars

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -3,7 +3,7 @@
 <x-panel class="bg-gray-100">
     <article class="flex bg-gray-100 space-x-4">
         <div class="flex-shrink-0">
-            <img src="https://i.pravatar.cc/60?u= {{$comment->id}}" alt="" width="60" height="60">
+            <img src="https://i.pravatar.cc/60?u={{$comment->user_id}}" alt="" width="60" height="60">
         </div>
 
         <div>
