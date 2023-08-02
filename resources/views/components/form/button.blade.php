a8c8333e6bb0a9c63d80dd93e68cddfa7ad24502
jose <jalves@gogreen.co.uk>
2023-08-02 09:08:26 +0100

    refactor: broke register page into components

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,6 +1,6 @@
 @props(['attributes'])
 
-<div class="flex justify-end mt-10 border-t border-gray=200 pt-6">
+<div class="flex justify-center mt-10 border-t border-gray=200 pt-6">
     <button type="submit"
             {{ $attributes(['class' => 'bg-blue-500 uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600']) }}>
         {{ $slot }}
